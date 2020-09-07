39d5a51f82c81d3fd95380c63a082c572994dab6
Guillem <gsantamaria@fc.ul.pt>
2020-09-07 10:19:02 +0200

    Delete all the things.

--- SPIA_analysis.R
+++
@@ -1,7 +0,0 @@
-setwd("/Users/santamag/Desktop/GUILLEM/wrkng_dirs_clean/SPIA")
-
-if (!requireNamespace("BiocManager", quietly = TRUE))
-        install.packages("BiocManager")
-
-if(!require(SPIA)) BiocManager::install("SPIA")
-library(SPIA)
